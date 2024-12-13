a1fbddf4e1ddb12bf0ec73fb27bd26322df093f0
YvesTheCode <alexandre.cleymans@gmail.com>
2024-12-13 18:18:43 +0100

    Le jeu marche

--- devinette.js
+++ devinette.js
@@ -34,18 +34,50 @@
 
 let min = 1;
 let max = 100;
-const nombreMystere = Math.floor(Math.random() * max) + min; // Nombre entre 1 et 100
+let nombreMystere = Math.floor(Math.random() * max) + min;
 let tentative = 0;
-let deviner = false;
 
-
+function reroll() {
+    nombreMystere = Math.floor(Math.random() * max) + min;}
 
 const annonce = document.getElementById("annonce");
 const entrer = document.getElementById("entrer");
 const input = document.getElementById("input");
+const indice = document.getElementById("indice");
+const essais = document.getElementById("essais");
 
-annonce.innerHTML="Bienvenue au jeu de devinette de nombre !"
-
+annonce.textContent="Bienvenue au jeu de devinette de nombre !"
+indice.textContent ="Il se trouve entre "+min+" & "+max
 input.value=""
 
 
+
+entrer.addEventListener("click",function(){
+
+    const tonchoix = input.value
+
+    ++tentative
+
+    alert(nombreMystere)
+    if (isNaN(tonchoix)) {
+        annonce.textContent="Devinette de >>>NOMBRE<<<"
+        indice.textContent="Trop nul"
+    } 
+    
+    else if(tonchoix < nombreMystere){
+        indice.textContent="trop petit"
+    }
+
+    else if(tonchoix > nombreMystere){
+        indice.textContent="trop grand"
+    }
+
+    else {
+        annonce.textContent="Tu as trouvé en seulement "+ tentative +" tentatives. Rejoue dés maintenant !"
+        indice.textContent="Trop fort"
+        nombreMystere = Math.floor(Math.random() * max) + min;
+        tentative=0
+        alert(nombreMystere)
+    }
+        essais.textContent = tentative
+});
